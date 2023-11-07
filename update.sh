774f7377d22217d9e5771648919f614a00637714
Feng Li <m@feng.li>
2023-11-07 16:44:53 +0800

    Update references

--- update.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-current_dir=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || /bin/pwd || pwd)
-
-GIT_DIR=$current_dir
-git commit -am "Update references"
-git push
