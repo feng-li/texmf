6918edbb63a87783f4e30295d9cf9653e1ab3280
Feng Li <m@feng.li>
2023-11-07 16:53:36 +0800

    revert

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #! /usr/bin/bash
 
 current_dir=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || /bin/pwd || pwd)
@@ -6,3 +7,13 @@ export GIT_DIR=${current_dir}/.git
 echo $GIT_DIR
 git commit -am "Update references"
 git push
+||||||| 774f737 (Update references)
+=======
+#! /usr/bin/bash
+
+current_dir=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || /bin/pwd || pwd)
+
+GIT_DIR=$current_dir
+git commit -am "Update references"
+git push
+>>>>>>> parent of 774f737 (Update references)
